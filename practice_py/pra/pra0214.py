8ae467752c3ed52e01e0e0e2f7c0943205fdb512
邢浩哲 <15044172+xinghaozhe@user.noreply.gitee.com>
2025-02-15 15:37:25 +0800

    0215_1 master commit

--- practice_py/pra/pra0214.py
+++ practice_py/pra/pra0214.py
@@ -1,5 +1,24 @@
-a=1
-b=2
-"黑手不能摸保险"
-"我爱鸽哥"
-"我爱鸽哥"
+import time
+import random
+
+class Account:
+    def __init__(self, name, password, money=0):
+        self.name = name
+        self.money = money
+        self.password=password
+
+        t=int(time.time()*100)
+        r=random.randint(10,99)
+        self.number=str(t)+str(r)
+
+    def savemoney(self,money):
+        self.money += money
+
+    def withdraw(self,money):
+        self.money -= money
+
+    def check(self):
+        print(f"账户{self.number}的当前余额为{self.money}")
+
+    def __del__(self):
+        print(f" 卡号为{self.number}的{self.name}已销户")
